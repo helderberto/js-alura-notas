267a9be537f9956414e35bfcf48cff26cb3e57c5
Helder Burato Berto <helder.burato@gmail.com>
2018-06-26 21:32:30 -0300

    Add count total by code using reduce

--- public/src/app.js
+++ public/src/app.js
@@ -3,7 +3,10 @@ import { handleStatus } from './utils/promise-helpers.js';
 document
 .querySelector('#myButton')
 .onclick = () => 
-  fetch('http://localhost:3000/notasx')
+  fetch('http://localhost:3000/notas')
   .then(handleStatus)
-  .then(notas => console.log(notas))
+  .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+  .then(itens => itens.filter(item => item.codigo == '2143'))
+  .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+  .then(console.log)
   .catch(console.log);
