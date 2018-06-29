ecb10ac4c4d0eb5233126dca9535301288884084
Helder Burato Berto <helder.burato@gmail.com>
2018-06-28 22:00:34 -0300

    Add operators partialize Update nota service break sumItems in small methods

--- public/src/app.js
+++ public/src/app.js
@@ -2,11 +2,6 @@ import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 
-const sumItems = code => notas => notas
-  .$flatMap(nota => nota.itens)
-  .filter(item => item.codigo == code)
-  .reduce((total, item) => total + item.valor, 0);
-
 document
 .querySelector('#myButton')
 .onclick = () => 
--- public/src/nota/service.js
+++ public/src/nota/service.js
@@ -1,17 +1,19 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
-const sumItems = code => notas => notas
-  .$flatMap(nota => nota.itens)
-  .filter(item => item.codigo == code)
-  .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
   listAll() {
     return fetch(API).then(handleStatus);
   },
   sumItems(code) {
+    const filterItems = partialize(filterItemsByCode, code);
+
     return this.listAll().then(sumItems(code));
   }
 };
