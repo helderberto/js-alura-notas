1cf19f1459f1d432ce08aa533b8312fbc4466ea7
Helder Burato Berto <helder.burato@gmail.com>
2018-06-28 22:05:03 -0300

    Update sumItems all small methods

--- public/src/nota/service.js
+++ public/src/nota/service.js
@@ -9,11 +9,23 @@ const sumItemsValue = items => items.reduce((total, item) => total + item.valor,
 
 export const notasService = {
   listAll() {
-    return fetch(API).then(handleStatus);
+    return fetch(API)
+      .then(handleStatus)
+      .catch(err => {
+        console.log(err);
+        return Promise.reject('Não foi possível obter as notas fiscais');
+      });
   },
   sumItems(code) {
     const filterItems = partialize(filterItemsByCode, code);
 
-    return this.listAll().then(sumItems(code));
+    return this.listAll()
+      .then(notas => 
+        sumItemsValue(
+          filterItems(
+            getItemsFromNotas(notas)
+          )
+        )
+      );
   }
 };
