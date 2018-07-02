e4b9275f580d6fb67c568aff1871c8846d064ca8
Helder Burato Berto <helder.burato@gmail.com>
2018-07-02 17:04:09 -0300

    Update execute operation using pipe and partialize

--- public/src/app.js
+++ public/src/app.js
@@ -1,15 +1,18 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
-const action = debounceTime(500, 
-  takeUntil(3, () => 
-    service
-    .sumItems('2143')
-    .then(log)
-    .catch(log)
-  )
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500),
+);
+
+const action = operations(() => 
+  service
+  .sumItems('2143')
+  .then(log)
+  .catch(log)
 );
 
 document
