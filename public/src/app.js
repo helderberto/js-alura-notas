6799eff338768ed8c007cdbbec1115952f5fe1c3
Helder Burato Berto <helder.burato@gmail.com>
2018-07-02 16:45:53 -0300

    Add debounceTime method Add limit of operations

--- public/src/app.js
+++ public/src/app.js
@@ -1,15 +1,17 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation = takeUntil(3, () => 
-  service
-  .sumItems('2143')
-  .then(log)
-  .catch(log)
+const action = debounceTime(500, 
+  takeUntil(3, () => 
+    service
+    .sumItems('2143')
+    .then(log)
+    .catch(log)
+  )
 );
 
 document
 .querySelector('#myButton')
-.onclick = operation;
+.onclick = action;
