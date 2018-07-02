7df79b399ab36972e36f0441395551258af77a53
Helder Burato Berto <helder.burato@gmail.com>
2018-07-02 17:39:59 -0300

    Add retry when offline

--- public/src/app.js
+++ public/src/app.js
@@ -1,4 +1,4 @@
-import { log, timeoutPromise, delay } from './utils/promise-helpers.js';
+import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
@@ -9,7 +9,7 @@ const operations = pipe(
 );
 
 const action = operations(() => 
-  timeoutPromise(200, service.sumItems('2143'))
+  retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
   .then(log)
   .catch(log)
 );
