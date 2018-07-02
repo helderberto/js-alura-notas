caa22481c6d95d6775ea2563740e41e876de6912
Helder Burato Berto <helder.burato@gmail.com>
2018-07-02 16:34:47 -0300

    Add takeUntil execute service max 3 times

--- public/src/app.js
+++ public/src/app.js
@@ -1,11 +1,15 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
 
-document
-.querySelector('#myButton')
-.onclick = () => 
+const operation = takeUntil(3, () => 
   service
   .sumItems('2143')
   .then(log)
-  .catch(log);
+  .catch(log)
+);
+
+document
+.querySelector('#myButton')
+.onclick = operation;
--- public/src/nota/service.js
+++ public/src/nota/service.js
@@ -1,5 +1,5 @@
 import { handleStatus } from '../utils/promise-helpers.js';
-import { partialize, compose } from '../utils/operators.js';
+import { partialize, pipe } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
@@ -18,7 +18,7 @@ export const notasService = {
   },
   sumItems(code) {
     const filterItems = partialize(filterItemsByCode, code);
-    
+
     const sumItems = pipe(
       getItemsFromNotas, 
       filterItems, 
